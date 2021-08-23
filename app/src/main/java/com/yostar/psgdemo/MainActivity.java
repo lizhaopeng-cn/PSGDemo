19c8dedb9ef3da79940fd5ca5dab8b24eeca9b99
lizhaopeng <zhaopeng.li@yo-star.com>
2021-08-23 18:38:56 +0800

    web oauth

--- app/src/main/java/com/yostar/psgdemo/MainActivity.java
+++ app/src/main/java/com/yostar/psgdemo/MainActivity.java
@@ -1,22 +1,19 @@
 package com.yostar.psgdemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.gms.auth.api.Auth;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.games.Games;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -26,24 +23,29 @@ public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient googleSignInClient;
     private GoogleSignInAccount googleSignInAccount;
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
 
                 signInOptions =
-                        new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+//                        new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 //                                .requestScopes(Games.SCOPE_GAMES_SNAPSHOTS)
                                 .requestId()
                                 .requestEmail()
-                                .requestIdToken("527953851962-5h94nh409p27hmjibmvv04fekv1nlhmb.apps.googleusercontent.com")
+                                .requestIdToken("527953851962-htan2je5kqu12htoabib8n2bkjqotmbt.apps.googleusercontent.com")
+//                                .requestIdToken("527953851962-5h94nh409p27hmjibmvv04fekv1nlhmb.apps.googleusercontent.com")
+//                                .requestIdToken("527953851962-8icfp8gn68hbno3oh66g3apn7tpun553.apps.googleusercontent.com")
+//                                .requestIdToken("273818645006-1f0kj4dhingkhbt4pderis2iho5gt2j3.apps.googleusercontent.com")
                                 .requestProfile()
-//                                .requestServerAuthCode("273818645006-1f0kj4dhingkhbt4pderis2iho5gt2j3.apps.googleusercontent.com")
+//                                .requestServerAuthCode("527953851962-8icfp8gn68hbno3oh66g3apn7tpun553.apps.googleusercontent.com")
                                 .build();
-//        signInOptions = GoogleSignInOptions.DEFAULT_SIGN_IN;
-//        signInOptions = GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN;
 
-        googleSignInClient = GoogleSignIn.getClient(MainActivity.this, signInOptions);
+        googleSignInClient = GoogleSignIn.getClient(this, signInOptions);
 
         findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,9 +100,7 @@ public class MainActivity extends AppCompatActivity {
      * 交互式登录
      */
     private void startSignInIntent() {
-        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
-                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
-        Intent intent = signInClient.getSignInIntent();
+        Intent intent = googleSignInClient.getSignInIntent();
         startActivityForResult(intent, RC_SIGN_IN);
     }
 
@@ -108,13 +108,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // The signed in account is stored in the result.
-                GoogleSignInAccount signedInAccount = result.getSignInAccount();
+//            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+//            if (result.isSuccess()) {
+//                // The signed in account is stored in the result.
+//                googleSignInAccount = result.getSignInAccount();
+//                Log.i("PSG", "getId:" + googleSignInAccount.getId());
+//                Log.i("PSG", "getIdToken:" + googleSignInAccount.getIdToken());
+//                Log.i("PSG", "getEmail:" + googleSignInAccount.getEmail());
+//                Log.i("PSG", "getAccount:" + googleSignInAccount.getAccount());
+//                Log.i("PSG", "getGrantedScopes:" + googleSignInAccount.getGrantedScopes());
+//                Log.i("PSG", "getDisplayName" + googleSignInAccount.getDisplayName());
+//                Log.i("PSG", "getFamilyName:" + googleSignInAccount.getFamilyName());
+//                Log.i("PSG", "getGivenName:" + googleSignInAccount.getGivenName());
+//                Log.i("PSG", "getServerAuthCode:" + googleSignInAccount.getServerAuthCode());
+//                Log.i("PSG", "getPhotoUrl:" + googleSignInAccount.getPhotoUrl());
+//                Log.i("PSG", "getRequestedScopes:" + googleSignInAccount.getRequestedScopes());
+//
+//                textView.append("getId:" + googleSignInAccount.getId() + "\n");
+//                textView.append("getIdToken:" + googleSignInAccount.getIdToken() + "\n");
+//                textView.append("getEmail:" + googleSignInAccount.getEmail() + "\n");
+//                textView.append("getAccount:" + googleSignInAccount.getAccount() + "\n");
+//                textView.append("getGrantedScopes:" + googleSignInAccount.getGrantedScopes() + "\n");
+//                textView.append("getDisplayName" + googleSignInAccount.getDisplayName() + "\n");
+//                textView.append("getFamilyName:" + googleSignInAccount.getFamilyName() + "\n");
+//                textView.append("getGivenName:" + googleSignInAccount.getGivenName() + "\n");
+//                textView.append("getServerAuthCode:" + googleSignInAccount.getServerAuthCode() + "\n");
+//                textView.append("getPhotoUrl:" + googleSignInAccount.getPhotoUrl() + "\n");
+//                textView.append("getRequestedScopes:" + googleSignInAccount.getRequestedScopes() + "\n");
+//            } else {
+//                Log.i("PSG", "auth fail");
+//            }
+
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInAccount result = null;
+            try {
+                googleSignInAccount = task.getResult(ApiException.class);
+                if (googleSignInAccount != null) {
                 Log.i("PSG", "getId:" + googleSignInAccount.getId());
-                Log.i("PSG", "getIdToken" + googleSignInAccount.getIdToken());
-                Log.i("PSG", "getEmail" + googleSignInAccount.getEmail());
+                Log.i("PSG", "getIdToken:" + googleSignInAccount.getIdToken());
+                Log.i("PSG", "getEmail:" + googleSignInAccount.getEmail());
                 Log.i("PSG", "getAccount:" + googleSignInAccount.getAccount());
                 Log.i("PSG", "getGrantedScopes:" + googleSignInAccount.getGrantedScopes());
                 Log.i("PSG", "getDisplayName" + googleSignInAccount.getDisplayName());
@@ -123,33 +156,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("PSG", "getServerAuthCode:" + googleSignInAccount.getServerAuthCode());
                 Log.i("PSG", "getPhotoUrl:" + googleSignInAccount.getPhotoUrl());
                 Log.i("PSG", "getRequestedScopes:" + googleSignInAccount.getRequestedScopes());
-            } else {
-                Log.i("PSG", "auth fail");
+
+
+                    textView.append("getId:" + googleSignInAccount.getId() + "\n");
+                    textView.append("getIdToken:" + googleSignInAccount.getIdToken() + "\n");
+                    textView.append("getEmail:" + googleSignInAccount.getEmail() + "\n");
+                    textView.append("getAccount:" + googleSignInAccount.getAccount() + "\n");
+                    textView.append("getGrantedScopes:" + googleSignInAccount.getGrantedScopes() + "\n");
+                    textView.append("getDisplayName" + googleSignInAccount.getDisplayName() + "\n");
+                    textView.append("getFamilyName:" + googleSignInAccount.getFamilyName() + "\n");
+                    textView.append("getGivenName:" + googleSignInAccount.getGivenName() + "\n");
+                    textView.append("getServerAuthCode:" + googleSignInAccount.getServerAuthCode() + "\n");
+                    textView.append("getPhotoUrl:" + googleSignInAccount.getPhotoUrl() + "\n");
+                    textView.append("getRequestedScopes:" + googleSignInAccount.getRequestedScopes() + "\n");
+                } else {
+                    showError(result);
+                }
+            } catch (ApiException e) {
+                e.printStackTrace();
             }
-
-
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            GoogleSignInAccount result = null;
-//            try {
-//                googleSignInAccount = task.getResult(ApiException.class);
-//                if (googleSignInAccount != null) {
-//                    Log.i("PSG", "getId:" + googleSignInAccount.getId());
-//                    Log.i("PSG", "getIdToken" + googleSignInAccount.getIdToken());
-//                    Log.i("PSG", "getEmail" + googleSignInAccount.getEmail());
-//                    Log.i("PSG", "getAccount:" + googleSignInAccount.getAccount());
-//                    Log.i("PSG", "getGrantedScopes:" + googleSignInAccount.getGrantedScopes());
-//                    Log.i("PSG", "getDisplayName" + googleSignInAccount.getDisplayName());
-//                    Log.i("PSG", "getFamilyName:" + googleSignInAccount.getFamilyName());
-//                    Log.i("PSG", "getGivenName:" + googleSignInAccount.getGivenName());
-//                    Log.i("PSG", "getServerAuthCode:" + googleSignInAccount.getServerAuthCode());
-//                    Log.i("PSG", "getPhotoUrl:" + googleSignInAccount.getPhotoUrl());
-//                    Log.i("PSG", "getRequestedScopes:" + googleSignInAccount.getRequestedScopes());
-//                } else {
-//                    showError(result);
-//                }
-//            } catch (ApiException e) {
-//                e.printStackTrace();
-//            }
 
 
 
